a4ab54e8db03cd3da3708e01e34356807b2afd9f
isaleemrana <saleemhur2020@gmail.com>
2023-06-05 03:18:40 +0500

    Uploading first site

---
+++ Dom.js
@@ -0,0 +1,12 @@
+// const home1=document.getElementById("home");
+// console.log(home1.textContent);
+
+// home1.textContent="Packages";
+// console.log(home1.textContent)
+
+const home1=document.getElementById("home");
+console.log(home1.innerText);
+
+home1.innerText="WELCOME";
+console.log(home1.innerText)
+
